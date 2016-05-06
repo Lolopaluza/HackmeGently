4ec11304f62a331fb821560e3c01951e735532f8
Ashley-Gautama <AshleyGautama.com>
2016-05-06 16:19:56 +0200

    fixed mistakes in skillscontroller

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,74 +1,45 @@
 class SkillsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /@skills
-  # GET /@skills.json
   def index
-    @skills = Skill.all
+    @skills = Skill.order(created_at: :asc)
+    render json: { skills: @skills }
   end
 
-  # GET /@skills/1
-  # GET /@skills/1.json
-  def show
-  end
-
-  # GET /@skills/new
-  def new
-    @skill = Skill.new
-  end
-
-  # GET /@skills/1/edit
-  def edit
-  end
-
-  # POST /@skills
-  # POST /@skills.json
   def create
     @skill = Skill.new(skill_params)
 
-    respond_to do |format|
-      if @skill.save
-        format.html { redirect_to @skill, notice: 'Skill was successfully created.' }
-        format.json { render :show, status: :created, location: @skill }
-      else
-        format.html { render :new }
-        format.json { render json: @skill.errors, status: :unprocessable_entity }
-      end
+    if @skill.save
+      render json: { skill: @skill, location: skill_url(@skill) }, status: :created # 201
+    else
+      render json: { errors: @skill.errors }, status: :unprocessable_entity # 422
     end
   end
 
-  # PATCH/PUT /@skills/1
-  # PATCH/PUT /@skills/1.json
+  def show
+    @skill = Skill.find(params[:id])
+    render json: { skill: @skill }
+  end
+
   def update
-    respond_to do |format|
-      if @skill.update(skill_params)
-        format.html { redirect_to @skill, notice: 'Skill was successfully updated.' }
-        format.json { render :show, status: :ok, location: @skill }
-      else
-        format.html { render :edit }
-        format.json { render json: @skill.errors, status: :unprocessable_entity }
-      end
+    @skill = Skill.find(params[:id])
+    if @skill.update(skill_params)
+      render json: { skill: @skill }, status: :accepted # 202
+    else
+      render json: { errors: @skill.errors }, status: :unprocessable_entity # 422
     end
   end
 
-  # DELETE /@skills/1
-  # DELETE /@skills/1.json
   def destroy
-    @skill.destroy
-    respond_to do |format|
-      format.html { redirect_to @skills_url, notice: 'Skill was successfully destroyed.' }
-      format.json { head :no_content }
+    @skill = Skill.find(params[:id])
+    if @skill.destroy
+      render json: { skill: nil }, status: :accepted # 202
+    else
+      render json: { errors: @skill.errors }, status: :unprocessable_entity # 422
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @skill = Skill.find(params[:id])
-    end
+  protected
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def skill_params
-      params.require(:skill).permit(:name, :user_id)
-    end
+  def skill_params
+    params.require(:skill).permit(:name)
   end
+end
