233c49676e0d4a7c3624be843668f657dd81bc5f
Samuel van Eck <samuelvaneck@gmail.com>
2016-05-06 13:15:24 +0200

    fixed migration issues

--- db/migrate/20160506095903_create_likes.rb
+++ db/migrate/20160506095903_create_likes.rb
@@ -1,8 +1,6 @@
 class CreateLikes < ActiveRecord::Migration
   def change
     create_table :likes do |t|
-      likeable_id :integer :index
-      likeable_type :string
       t.timestamps null: false
     end
   end
