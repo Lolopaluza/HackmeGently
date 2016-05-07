a7a551bb8f58aa11a98225d0fc0ece3af709dab9
lolopaluza <upsonmanoah@gmail.com>
2016-05-07 12:43:09 +0200

    seeds data made more funny

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,54 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-  # Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete
+
+  User.create({ email:"billgates@microsoft.com", password:"abcd1234"})
+  User.create({ email:"tesla@genius.com", password:"abcd1234"})
+  User.create({ email:"zedshawk@python.com", password:"abcd1234"})
+  User.create({ email:"teamawesome@hackmegently.com", password:"abcd1234"})
+  User.create({ email:"stevejobs@swift.com", password:"abcd1234"})
+  User.create({ email:"CSSfan@styling.com", password:"abcd1234"})
+  User.create({ email:"newbyHTMLfan@idontknowwhatimdoing.com", password:"abcd1234"})
+  User.create({ email:"Javafan@moneyyy.com", password:"abcd1234"})
+  User.create({ email:"alanturing@turingcomps.com", password:"abcd1234"})
+  User.create({ email:"zuckerburg@fb.com", password:"abcd1234"})
 
 
-  User.create({ email:"aha@lol.com", password:"abcd1234"})
-  User.create({ email:"frank@lol.com", password:"abcd1234"})
-  User.create({ email:"boblol.com", password:"abcd1234"})
-  User.create({ email:"steven@lol.com", password:"abcd1234"})
-  User.create({ email:"guile@lol.com", password:"abcd1234"})
-  User.create({ email:"marklol.com", password:"abcd1234"})
+Post.delete
 
-  Post.create({ content: "react on rails gem rocks", user_id:1})
-  Post.create({ content: "Javascript for the win", user_id:1})
-  Post.create({ content: "Good old fashioned C", user_id:2})
-  Post.create({ content: "Ruby language is for sissies", user_id:3})
+  Post.create({ content: "Hey jobs, what are your thoughts on this blue box?", user_id:1})
+  Post.create({ content: "Anyone here know about a devise to make my shit work?", user_id:2})
+  Post.create({ content: "In need of faster cpu, the germans are coming :(", user_id:9})
+  Post.create({ content: "Ruby is dead", user_id:3})
+  Post.create({ content: "The react guide for noobs", user_id:4})
+  Post.create({ content: "Your all wrong..", user_id:5})
+  Post.create({ content: "Style Style Style, the generic website guide", user_id:6})
+  Post.create({ content: "How do i make websites shiny", user_id:7})
+  Post.create({ content: "I'm a genius, let me tell you about it", user_id:10})
+
+
+Skill.delete
 
   Skill.create({ name: "C#"})
   Skill.create({ name: "C"})
   Skill.create({ name: "Javascript"})
   Skill.create({ name: "Ruby"})
-  Skill.create({ name: "Ruby on rails"})
-  Skill.create({ name: "Microsoft MVC"})
+  Skill.create({ name: "Python"})
+  Skill.create({ name: "React"})
+  Skill.create({ name: "Fortran"})
+  Skill.create({ name: "SQL"})
+  Skill.create({ name: "Java"})
+  Skill.create({ name: "CSS"})
+  Skill.create({ name: "HTML"})
 
-  Comment.create({ text:"I agree", post_id:1, user_id:1})
-  Comment.create({ text:"totally wrong", post_id:1, user_id:2})
-  Comment.create({ text:"nope", post_id:2, user_id:3})
-  Comment.create({ text:"thats just cruel...", post_id:4, user_id:4})
+Comment.delete
+
+  Comment.create({ text:"Hahahaha *flip table* and ruuuunnnn", post_id:1, user_id:5})
+  Comment.create({ text:"I'm working on it, in the meantime, you want somebody to work you? ;)", post_id:2, user_id:9})
+  Comment.create({ text:"Check out my latest model it's the bomb", post_id:3, user_id:1})
+  Comment.create({ text:"In the sence that it will blow up in your face, yes indeed", post_id:3, user_id:5})
+  Comment.create({ text:"Stfu", post_id:4, user_id:4})
+  Comment.create({ text:"You dont know what your talking about", post_id:5, user_id:10})
+  Comment.create({ text:"Well screw you too.", post_id:5, user_id:4})
+  Comment.create({ text:"If i had one ball i would still be more capable than you", post_id:6, user_id:3})
+  Comment.create({ text:"You need to use google", post_id:7, user_id:4})
+  Comment.create({ text:"Dont make a website, check out mine and be social instead!", post_id:8, user_id:10})
+  Comment.create({ text:"I'm a genius, dont see you winning any wars", post_id:9, user_id:3})
