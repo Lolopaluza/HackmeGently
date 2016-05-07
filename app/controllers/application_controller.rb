ae74c36689594719eda4a3d4dc87385e834c58a1
Samuel van Eck <samuelvaneck@gmail.com>
2016-05-07 12:16:53 +0200

    created posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   skip_before_action :verify_authenticity_token
-  before_action :configure_permitted_parameters, if: :devise_controller?, :authenticate_user!
+  before_action :authenticate_user!
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
