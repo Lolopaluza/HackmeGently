a439ea38e95e6c5ca163a378a2e97b13851288c6
durw4rd <misa.fasa@gmail.com>
2016-05-07 13:36:20 +0200

    add material-ui dependency; basic navbar

--- client/app/bundles/HackmeGently/containers/HackmeGently.jsx
+++ client/app/bundles/HackmeGently/containers/HackmeGently.jsx
@@ -2,12 +2,20 @@ import React, { PropTypes } from 'react';
 import HackmeGentlyWidget from '../components/HackmeGentlyWidget';
 import _ from 'lodash';
 
+import ThemeManager from 'material-ui/lib/styles/theme-manager';
+import Theme from '../lib/Theme';
+import Colors from 'material-ui/lib/styles/colors';
+
+import AppBarComponent from '../components/AppBarComponent';
+
+
 // Simple example of a React "smart" component
 export default class HackmeGently extends React.Component {
 
   render() {
     return (
       <div>
+        <AppBarComponent />
         {this.props.children}
       </div>
     );
